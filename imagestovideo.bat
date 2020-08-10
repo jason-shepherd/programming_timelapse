0ed7d7a22a79aa5ee528c438d5fa15ad8aeec4b2
Jason Shepherd <jmanshepherd19@gmail.com>
2020-08-09 22:35:11 -0400

    refactored and redid everything code is still gross tho

--- imagestovideo.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set /p framerate=Enter framerate:
-set /p directory=Enter directory:
-for /r %%a in (%directory%\*.png) do (
-    echo file '%%a' >> images.txt
-)
-ffmpeg.exe -r %framerate% -f concat -safe 0 -i images.txt -c:v libx264 -pix_fmt yuv420p %directory%\video.mp4
-del /q images.txt
